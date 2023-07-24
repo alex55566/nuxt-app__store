8dc6d6d6f99069aaa588d5936235273d5c51f7f0
Natalya Pirogova <npirogova@phoenixit.ru>
2023-07-24 23:19:47 +0300

    nuxt

---
+++ dist/_nuxt/static/1690229774/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-app\u002F_nuxt\u002Fstatic\u002F1690229774",layout:"default",error:a,state:{fruits:[1,2,3,4,5]},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-app\u002F",assetsPath:"\u002Fnuxt-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
